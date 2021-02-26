d37776e5627d661f180dc375c90725c944b305c1
Color0525 <rio.game.kojima@gmail.com>
2021-02-26 20:15:49 +0900

    BGMの追加

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// オーディオを管理
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] AudioClip m_intro;
+    [SerializeField] AudioClip m_loop;
+
+    AudioSource m_audio;
+
+    void Start()
+    {
+        m_audio = GetComponent<AudioSource>();
+        StartCoroutine(PlayBGM());
+    }
+
+    IEnumerator PlayBGM()
+    {
+        if (m_intro)
+        {
+            m_audio.clip = m_intro;
+            m_audio.loop = false;
+            m_audio.Play();
+
+            while (m_audio.isPlaying)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+        }
+
+        m_audio.clip = m_loop;
+        m_audio.loop = true;
+        m_audio.Play();
+    }
+}
